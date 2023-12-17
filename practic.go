70367d1d4811fada0160f5b1bbb9e2674c34a246
Guvanchhojamov <name@mail.com>
2023-12-17 17:58:37 +0500

    wrap error examples

--- practic.go
+++ practic.go
@@ -4,4 +4,32 @@ import "fmt"
 
 func main() {
 	fmt.Println("some practics")
+	//if err := foo.Open("foo"); err != nil {
+	//	if foo.IsNotFoundError(err) {
+	//		// handle
+	//	} else {
+	//		panic("unknown error")
+	//	}
+	//}
 }
+
+// package foo
+
+type errNotFound struct {
+	file string
+}
+
+func (e errNotFound) Error() string {
+	return fmt.Sprintf("file %q not found", e.file)
+}
+
+func IsNotFoundError(err error) bool {
+	_, ok := err.(errNotFound)
+	return ok
+}
+
+func Open(file string) error {
+	return errNotFound{file: file}
+}
+
+// package bar
