9d13e1abe0cea6f40fabe34d6e2a0211a413bc37
Guvanchhojamov <name@mail.com>
2023-12-17 18:46:18 +0500

    removed old

--- practic.go
+++ practic.go
@@ -3,33 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("some practics")
-	//if err := foo.Open("foo"); err != nil {
-	//	if foo.IsNotFoundError(err) {
-	//		// handle
-	//	} else {
-	//		panic("unknown error")
-	//	}
-	//}
+	fmt.Println("some practic")
 }
-
-// package foo
-
-type errNotFound struct {
-	file string
-}
-
-func (e errNotFound) Error() string {
-	return fmt.Sprintf("file %q not found", e.file)
-}
-
-func IsNotFoundError(err error) bool {
-	_, ok := err.(errNotFound)
-	return ok
-}
-
-func Open(file string) error {
-	return errNotFound{file: file}
-}
-
-// package bar
